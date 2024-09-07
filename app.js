5af59101ee9791792808b1803c48d3a7ebc12fdb
danielecruzbr <danielecruzsilveira@gmail.com>
2024-09-07 03:21:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+// Esse arquivo contém a lógica.
+
+/* Declara uma função chamada pesquisar.
+Ao clicar em "Buscar", a função pesquisar() é chamada.
+O loop for...of itera sobre os resultados da pesquisa.
+Para cada resultado, um novo elemento HTML é criado (uma div com as classes e conteúdos necessários).
+O HTML gerado para todos os resultados é concatenado em uma única string.
+Essa string é atribuída à propriedade innerHTML da seção "resultados-pesquisa", substituindo o conteúdo anterior.*/
+function pesquisar() {
+
+// Essa linha busca no documento HTML(document) um elemento com o ID "resultados-pesquisa"(getElementById("")) e armazena-o na variável section (criada pelo let).
+let section = document.getElementById("resultados-pesquisa")
+// Busca o elemento HTML com o ID "campo-pesquisa" e armazena o valor que o usuário digitou nesse campo na variável campoPesquisa.
+let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+// A condição if (campoPesquisa === "") verifica se o valor do campo de pesquisa está vazio. Se estiver, o código dentro do bloco if será executado.
+if (campoPesquisa == "") {
+    // Atribui o HTML com a mensagem de erro ao conteúdo da seção.
+    section.innerHTML = "<p>Nenhum conteúdo encontrado.</p>";
+        // encerrar função
+        return
+    }
+
+//* Exibe no console do navegador o objeto section (opcional).
+console.log(section,campoPesquisa);
+
+campoPesquisa = campoPesquisa.toLowerCase()
+
+// Inicializa uma string vazia para armazenar os resultados.
+let resultados = "";
+let titulo = "";
+
+//  Essa variável é inicializada com 0 e incrementada a cada resultado encontrado
+let resultadosEncontrados = 0;
+
+// Este é um loop for...of que itera sobre um conjunto de dados (representado pela variável dados). A cada iteração, a variável dado receberá um elemento desse conjunto.
+for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    //condicional, se o título includes campoPesquisa
+    if(dado.titulo.includes(campoPesquisa)){
+        // Cria um novo elemento html para cada resultado.
+    resultados += `
+    <div class="item-resultado">
+        <h3>
+            <a href=${dado.link} target="_blank">${dado.titulo}</a>
+        </h3>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Saiba mais.</a>
+    </div>
+`;
+    // Incrementa a contagem de resultados
+    resultadosEncontrados++; 
+    }  
+}
+// Verifica se algum resultado foi encontrado
+if (resultadosEncontrados === 0) {
+    section.innerHTML = `<p>Nenhum resultado encontrado para a pesquisa "${campoPesquisa}".</p>`;
+  } else {
+// Interação entre JavaScript e HTML. innerHTML permite modificar o conteúdo HTML dentro do elemento section.
+section.innerHTML = resultados
+  }
+}
---
+++ dados.js
@@ -0,0 +1,54 @@
+//Esse arquivo contém as informações.
+
+/* let cria uma variável. dados é o nome da variável. = atribui um valor à variável dados. O valor está entre "", neste caso. [] representa um Array/Lista: uma coleção ordenada de valores. {} Cria um objeto, com informações referentes aos artistas.*/
+let dados = [
+    {
+        titulo : "Aleijadinho",
+        descricao : "Antônio Francisco Lisboa, o Aleijadinho, foi um escultor e arquiteto brasileiro que viveu de 1738 a 1814. Ele nasceu em Ouro Preto, Minas Gerais, e é considerado um dos artistas mais importantes da história brasileira. Ele é conhecido por suas esculturas expressivas e dramáticas com temas religiosos, feitas especialmente em pedra-sabão. Suas obras mais famosas incluem as esculturas no Santuário de Bom Jesus de Matosinhos em Congonhas, Minas Gerais, e a fachada da Igreja de São Francisco de Assis em Ouro Preto. A vida de Aleijadinho foi cheia de desafios. O artista foi atingido por uma doença que lhe causou deformidade e dificultava sua mobilidade. Apesar disso, ele foi capaz de superar suas limitações físicas, tornando-se um dos maiores artistas da história brasileira.",
+        influencias : "Barroco",
+        link : "http://portal.iphan.gov.br/uploads/publicacao/ColRotPat1_AleijadinhoSantuarioCongonhas.pdf",
+    },
+    {
+        titulo : "Anita Malfatti",
+        descricao : "Anita Malfatti (1889-1964) é considerada uma das figuras mais importantes do modernismo brasileiro. Ela nasceu em São Paulo, e estudou arte na Alemanha e nos Estados Unidos. Suas obras foram influenciadas pelo expressionismo e o cubismo, com o uso de cores vívidas, retratando especialmente cenas da vida cotidiana e elementos da cultura brasileira. Ela também pintou muitos retratos e paisagens. Algumas de suas obras mais importantes são A Boba (1915), O Homem Amarelo (1915), A Mulher de Cabelos Verdes (1915), O Farol (1915), Porto de Mônaco (1925) e Tropical (1917). Seu trabalho sofreu críticas quando foi exibido pela primeira vez no Brasil, por ser muito vanguardista e não seguir as regras tradicionais da pintura. Ela promoveu uma renovação nas artes plásticas, sendo pioneira do movimento modernista no Brasil.",
+        influencias : "Expressionismo, Modernismo",
+        link : "https://artsandculture.google.com/entity/m08c9kt?hl=pt-br"
+    },
+    {
+        titulo : "Artur Timóteo da Costa",
+    },
+    {
+        titulo : "Beatriz Milhazes",
+    },
+    {
+        titulo: "Cândido Portinari",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu nisl lectus. Nullam mattis, ligula non placerat eleifend, ante elit dictum turpis, dignissim tristique justo felis eu erat.",
+        link : "https://www.portinari.org.br/"
+    },
+    {
+        titulo : "Di Cavalcanti",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu nisl lectus. Nullam mattis, ligula non placerat eleifend, ante elit dictum turpis, dignissim tristique justo felis eu erat.",
+        link : "https://artsandculture.google.com/entity/m08zflt?hl=pt"
+    },
+    {
+        titulo : "Lygia Clark",
+    },
+    {
+        titulo: "Maria Auxiliadora",
+    },
+    {
+        titulo : "Tarsila do Amaral",
+        descricao : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu nisl lectus. Nullam mattis, ligula non placerat eleifend, ante elit dictum turpis, dignissim tristique justo felis eu erat.",
+        link : "https://www.tarsiladoamaral.com.br/"
+    },
+    {
+        titulo : "Vicente do Rego Monteiro",
+    },
+    {
+        titulo : "Victor Meirelles",
+    },
+    {
+        titulo : "Vik Muniz",
+    }
+
+];
